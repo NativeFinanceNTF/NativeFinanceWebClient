747816523cbfea34cf93c7bd9ed54ffe2a663840
Lala <lala@h-MacBook-Pro-sl-Tsur.local>
2022-06-19 20:09:46 +0300

    Updates

---
+++ static/js/24.4791043f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnative-frontend"]=this["webpackJsonpnative-frontend"]||[]).push([[24],{1322:function(n,t){},1372:function(n,t){},1374:function(n,t){},1407:function(n,t){},1425:function(n,t){},1427:function(n,t){}}]);
+//# sourceMappingURL=24.4791043f.chunk.js.map
